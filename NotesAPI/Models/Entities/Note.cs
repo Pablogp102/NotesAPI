74ebea08398fb44de105f32ffce47251c4471eab
Pablogp102 <pablopg102@gmail.com>
2024-05-01 16:18:16 +0200

    Add project files.

---
+++ NotesAPI/Models/Entities/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace NotesAPI.Models.Entities
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public bool IsVisible { get; set; }
+    }
+}
